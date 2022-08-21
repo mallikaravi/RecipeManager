bcdea41358f41b5213802710391abd29a267fd77
mallikaravi <mailtomallikaravi@gmail.com>
2022-08-21 13:20:25 +0200

    Dietician MVC darft

---
+++ src/com/novare/recipe/service/IDieticianService.java
@@ -0,0 +1,7 @@
+package com.novare.recipe.service;
+
+import com.novare.recipe.model.Recipe;
+
+public interface IDieticianService {
+	Recipe createRecipe(Recipe recipe);
+}
---
+++ src/com/novare/recipe/service/impl/DieticianServiceImpl.java
@@ -0,0 +1,14 @@
+package com.novare.recipe.service.impl;
+
+import com.novare.recipe.model.Recipe;
+import com.novare.recipe.service.IDieticianService;
+
+public class DieticianServiceImpl implements IDieticianService{
+
+	@Override
+	public Recipe createRecipe(Recipe recipe) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
