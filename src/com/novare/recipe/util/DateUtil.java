c7c4da6cf3b83b46b525f31d4d6e275d150af860
mallikaravi <mailtomallikaravi@gmail.com>
2022-08-24 16:58:19 +0200

    Improved Date Util

--- src/com/novare/recipe/util/DateUtil.java
+++ src/com/novare/recipe/util/DateUtil.java
@@ -1,32 +1,67 @@
 package com.novare.recipe.util;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.temporal.ChronoField;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.time.temporal.WeekFields;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public final class DateUtil {
 
 	private final static String DATE_FORMAT_PATTERN = "dd-MM-yyyy";
 
-	public static Integer getWeekNumber(int year, int month, int day) {
-		LocalDate date = LocalDate.of(year, month, day);
-		int weekOfYear = date.get(ChronoField.ALIGNED_WEEK_OF_YEAR);
+	public static LocalDate toLocalDate(Date date) {
+		return LocalDate.ofInstant(date.toInstant(), ZoneId.systemDefault());
+	}
+
+	public static Date toDate(LocalDate date) {
+		return Date.from(date.atStartOfDay(ZoneId.systemDefault()).toInstant());
+	}
+
+	public static Integer getWeekNumberOn(LocalDate date) {
+		int weekOfYear = date.get(WeekFields.of(date.getDayOfWeek(), 2).weekOfYear());
 		return weekOfYear;
 	}
 
 	public static Integer getWeekNumberNow() {
-		LocalDate date = LocalDate.now();
-		int weekOfYear = date.get(ChronoField.ALIGNED_WEEK_OF_YEAR);
-		return weekOfYear;
+		return getWeekNumberOn(LocalDate.now());
 	}
 
-	public static Date getDate(String dateAsString) throws ParseException {
-		return new SimpleDateFormat(DATE_FORMAT_PATTERN).parse(dateAsString);
+	public static int getDayNumber(LocalDate date) {
+		DayOfWeek day = date.getDayOfWeek();
+		return day.getValue();
+	}
+
+	public static String getDayString(LocalDate date) {
+		DayOfWeek day = date.getDayOfWeek();
+		return day.getDisplayName(TextStyle.FULL, Locale.getDefault());
+	}
+
+	public static List<String> getWeekDays() {
+		List<String> weekDays = new ArrayList<>();
+		for (DayOfWeek dayOfWeek : DayOfWeek.values()) {
+			String format = String.format("%-9s {%-10s}", dayOfWeek.getDisplayName(TextStyle.FULL, Locale.getDefault()),
+					LocalDate.now().with(dayOfWeek));
+			weekDays.add(format);
+		}
+		return weekDays;
+	}
+
+	public static LocalDate toDate(String dateAsString) throws ParseException {
+		return LocalDate.parse(dateAsString, DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN));
+	}
+
+	public static String toString(LocalDate date) throws ParseException {
+		return date.format(DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN));
 	}
 
 	public static String getNow() throws ParseException {
-		return new SimpleDateFormat(DATE_FORMAT_PATTERN).format(new Date());
+		return toString(LocalDate.now());
 	}
 }
